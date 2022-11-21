1027fa104f27707f12b3d8400e5f4ea58ec586d4
JJerryJi <jerryji040506@ucla.edu>
2022-11-20 21:05:17 -0800

    Update with Chakra UI

--- src/Components/Square.js
+++ src/Components/Square.js
@@ -1,5 +1,5 @@
 import React from "react"
-import "../App.css"
+import "../Components/Game.css"
 function Square({val,chooseSquare,}){
     return (
         <div className="square" onClick={chooseSquare}> {val} </div>
