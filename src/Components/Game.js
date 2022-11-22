22597caa143004d31a8b19722b4ace9cc1f15a71
JJerryJi <jerryji040506@ucla.edu>
2022-11-21 18:44:41 -0800

    Update the game using SweetAlert

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -2,9 +2,12 @@ import './Game.css';
 import { useState, useEffect } from "react";
 import Square from './Square'
 import {Patterns} from "../Patterns"
+import { Box,Heading,Center } from '@chakra-ui/react'
+import swal from 'sweetalert';
 
 function GameStart() {
 
+  
   const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
   const [player,setPlayer] = useState("O");
   const [result, SetResult] = useState({winner:"none",state: "none"});
@@ -21,7 +24,26 @@ function GameStart() {
 
   useEffect(()=>{
     if(result.state != "none") {
-      alert(`Game Finished! Winning Player: ${result.winner}`);
+      swal(`Game Finished! Winning Player: ${result.winner}`,{
+        buttons: {
+          catch: {
+            text: "Confirm!",
+            value: "catch",
+          },
+          cancel: true,
+        },
+      })
+      .then((value) => {
+        switch (value) {
+       
+          case "catch":
+            swal("Gotcha!","Time to Celebrate", "success");
+            break;
+       
+          default:
+            swal("Let's Startover");
+        }
+      });
     }
     restartGame();
   },[result])
@@ -72,26 +94,30 @@ function GameStart() {
     setBoard(["", "", "", "", "", "", "", "", ""]);
     setPlayer("O");
   }
+
   return (
-  <div className="App">
-    <div className="board">
-      <div className='row'> 
+  <Box p={10}>
+    <Center p={5}> <Heading>Tic-Tac-Toe Game</Heading> </Center>
+    <Center>
+    <Box className="board">
+      <Box className='row'> 
         <Square val={board[0]} chooseSquare={() => {chooseSquare(0)}}/>
         <Square val={board[1]} chooseSquare={() => {chooseSquare(1)}}/>
         <Square val={board[2]} chooseSquare={() => {chooseSquare(2)}}/>
-       </div>
-      <div className='row'> 
+       </Box>
+      <Box className='row'> 
         <Square val={board[3]} chooseSquare={() => {chooseSquare(3)}}/>
         <Square val={board[4]} chooseSquare={() => {chooseSquare(4)}}/>
         <Square val={board[5]} chooseSquare={() => {chooseSquare(5)}}/>
-      </div>
-      <div className='row'>
+      </Box>
+      <Box className='row'>
         <Square val={board[6]} chooseSquare={() => {chooseSquare(6)}}/>
         <Square val={board[7]} chooseSquare={() => {chooseSquare(7)}}/>
         <Square val={board[8]} chooseSquare={() => {chooseSquare(8)}}/>
-      </div>
-    </div>
-  </div>
+      </Box>
+    </Box>
+    </Center>
+  </Box>
   )
   ;
 }
